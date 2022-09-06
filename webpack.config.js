8471eca5a6a70a3ee9b9a84dc3a6f74ca54ebefd
Gustavo Leyte-Vidal Chacón <glcdmx@gmail.com>
2022-09-06 06:36:59 -0400

    feat(Root): Prepare for production

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
 	entry: "./src/index.jsx",
@@ -68,6 +69,7 @@ module.exports = {
 				},
 			],
 		}),
+		new CleanWebpackPlugin(),
 	],
 	devServer: {
 		historyApiFallback: true,
